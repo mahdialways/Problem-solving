34e5e94eb49a3d089187402beed8ca6681d040eb
Hasan Al Mahdi <mahdialwaysworking@gmail.com>
2025-10-17 19:55:11 +0600

    Create height_of_rational.c

---
+++ height_of_rational.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a>b){
+        printf("%d",a);
+    }
+    else{
+        printf("%d",b);
+    }
+}
