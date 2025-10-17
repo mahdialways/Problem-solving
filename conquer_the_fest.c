e2b1b4819e74fa92d230c1d929278e3f34e293cb
Hasan Al Mahdi <mahdialwaysworking@gmail.com>
2025-10-17 09:56:08 +0600

    Create conquer_the_fest.c

---
+++ conquer_the_fest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    c = 10 *b;
+    
+    if(a >= c){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+}
+
