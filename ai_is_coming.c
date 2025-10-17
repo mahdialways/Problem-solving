ea1e188bbaa5c77b3a4ad24f75cc821926acb313
Hasan Al Mahdi <mahdialwaysworking@gmail.com>
2025-10-17 10:15:31 +0600

    Create ai_is_coming.c

---
+++ ai_is_coming.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int x;
+    scanf("%d",&x);
+    if(x<= 60){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
+
