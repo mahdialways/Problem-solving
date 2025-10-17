37060f5450bf0f28ec49b4bf425d4b58b860c534
Hasan Al Mahdi <mahdialwaysworking@gmail.com>
2025-10-17 10:23:14 +0600

    Create iceream.c

---
+++ iceream.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int x,y;
+    scanf("%d %d",&x,&y);
+    if(x*2 <= y){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
+
